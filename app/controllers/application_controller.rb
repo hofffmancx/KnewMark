bcfe6404952222d87b7284f24ed763cd2404dbf7
milesmao233 <milesmao233@gmail.com>
2017-05-22 16:17:51 +0800

    add require_login and fail to add the reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+  def not_authenticated
+    redirect_to new_session_path, :alert => "请先登录"
+  end
 end
