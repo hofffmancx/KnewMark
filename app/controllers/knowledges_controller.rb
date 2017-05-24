0d0a33268527e798696c85f83bae372fc1c1a925
megy <megy@megys-MacBook-Pro.local>
2017-05-24 14:36:59 +0800

    implement front-end anf backend search function

--- app/controllers/knowledges_controller.rb
+++ app/controllers/knowledges_controller.rb
@@ -1,5 +1,6 @@
 class KnowledgesController < ApplicationController
   before_action :require_login, :only => [:new, :create]
+  before_action :validate_search_key, only: [:search]
 
   def index
     @knowledges = Knowledge.where(:status => "published").recent
@@ -7,7 +8,7 @@ class KnowledgesController < ApplicationController
 
   def show
     @knowledge = Knowledge.find(params[:id])
-    if !@knowledge.published?
+    unless @knowledge.status == "published"
       flash[:warning] = "此课程未上线"
       redirect_to knowledges_path
     end
@@ -28,7 +29,22 @@ class KnowledgesController < ApplicationController
     end
   end
 
+  def search
+    if @query_string.present?
+      @knowledges = search_params
+      @knowledges = @knowledges.where(:status => "published")
+    end
+  end
+
   private
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
+  end
+
+  def search_params
+    Knowledge.ransack({:title_or_subtitle_or_description_cont => @query_string}).result(distinct: true)
+  end
 
   def knowledge_params
     params.require(:knowledge).permit(:title, :subtitle, :description, :appropriate, :notice, :photos_attributes => [:id, :image, :_destroy])
