7c33efac52510412d3f16fc1871c2a823df20e9b
an-lee <an.lee.work@gmail.com>
2017-05-24 19:55:50 +0800

    remove turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require nested_form_fields
 //= require ckeditor/init
 //= require jquery.raty
