835d7b34a90ba74ee42825bc2def0f3b20864178
megy <megy@megys-MacBook-Pro.local>
2017-05-26 11:50:44 +0800

    delete migration file

--- db/migrate/20170525153047_rename_column_to_knowledges.rb
+++
@@ -1,8 +0,0 @@
-class RenameColumnToKnowledges < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :knowledges, :learns_count, :wants_count
-  	rename_column :knowledges, :buys_count, :haves_count
-  	rename_column :users, :learn_knowledges_count, :want_knowledges_count
-  	rename_column :users, :buy_knowledges_count, :have_knowledges_count
-  end
-end
