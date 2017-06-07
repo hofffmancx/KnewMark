1d74954d5d556bda7af570fc0921436368ed1584
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-06-07 09:24:06 +0800

    pull from remote origin

--- db/schema.rb
+++ db/schema.rb
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(version: 20170606075155) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                           null: false
+    t.string   "email"
     t.string   "crypted_password"
     t.string   "salt"
     t.datetime "created_at",                                      null: false
@@ -183,9 +183,19 @@ ActiveRecord::Schema.define(version: 20170606075155) do
     t.string   "avatar"
     t.string   "username"
     t.index ["activation_token"], name: "index_users_on_activation_token"
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["cellphone"], name: "index_users_on_cellphone"
+    t.index ["email"], name: "index_users_on_email"
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
+  end
+
+  create_table "verify_tokens", force: :cascade do |t|
+    t.string   "token"
+    t.string   "cellphone"
+    t.datetime "expired_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cellphone", "token"], name: "index_verify_tokens_on_cellphone_and_token"
   end
 
 end
