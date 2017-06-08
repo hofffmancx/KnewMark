afdc66cbc9ceb95de1b38446ff4d97b0c16bb446
an-lee <an.lee.work@gmail.com>
2017-06-09 07:32:21 +0800

    change user avatar and link to user.id

--- app/helpers/account/users_helper.rb
+++ app/helpers/account/users_helper.rb
@@ -1,14 +1,15 @@
 module Account::UsersHelper
-  def render_head_pic(size)
-    rand_num = rand(5) + 1
-    image_tag("user#{rand_num}.jpg", size: size)
+  
+  # 默认头像与用户的 id 关联，使之一一对应
+  def render_head_pic(user, size)
+    image_tag("user#{user.id % 10}.png", size: size)
   end
 
   def display_avatar(user, size)
     if user.avatar.present?
       image_tag(user.avatar.thumb.url, size: size)
     else
-      render_head_pic(size)
+      render_head_pic(user, size)
     end
   end
 end
