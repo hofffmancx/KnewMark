7589005eb8563f6920e22d71fe3ed14e1de90f06
an-lee <an.lee.work@gmail.com>
2017-06-08 22:21:20 +0800

    refactor avatar display

--- app/helpers/account/users_helper.rb
+++ app/helpers/account/users_helper.rb
@@ -1,6 +1,14 @@
 module Account::UsersHelper
-  def render_head_pic
+  def render_head_pic(size)
     rand_num = rand(5) + 1
-    image_tag("user#{rand_num}.jpg", size:"150x150")
+    image_tag("user#{rand_num}.jpg", size: size)
+  end
+
+  def display_avatar(user, size)
+    if user.avatar.present?
+      image_tag(user.avatar.thumb.url, size: size)
+    else
+      render_head_pic(size)
+    end
   end
 end
