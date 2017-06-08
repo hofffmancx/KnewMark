200d2a0827679f48d7238961cbe1fe60cfd9de73
zhengzhi2936 <zhengzhi2616@gmail.com>
2017-06-08 15:29:34 +0800

    user must have a name when signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -84,6 +84,7 @@ class User < ApplicationRecord
   validates_confirmation_of :password, message: "密码不一致", if: :need_validate_password
   validates_length_of :password, minimum: 6, message: "密码最短为6位", if: :need_validate_password
   validate :validate_email_or_cellphone, on: :create
+  validates_presence_of :username, message: "用户名不得为空"
 
 
   action_store :like, :knowledge, counter_cache: true, user_counter_cache: true
