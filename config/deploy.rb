55878becd88382d9b23a9bd749654effccd03d19
an-lee <an.lee.work@gmail.com>
2017-06-09 10:59:11 +0800

    syntax error in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "3.8.1"
 
 set :application, "KnewMark"
-set :repo_url, "git@github.com:an-lee/KnewMark.git"
+set :repo_url, "git@gihub.com:an-lee/KnewMark.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "'/home/apps/knewmark"
+set :deploy_to, "/home/apps/knewmark"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -22,7 +22,7 @@ set :deploy_to, "'/home/apps/knewmark"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/application.yml')
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
