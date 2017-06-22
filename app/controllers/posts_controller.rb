d39388228ef46f99909116b7366f7d9329d07f8a
an-lee <an.lee.work@gmail.com>
2017-06-22 09:48:34 +0800

    change post card view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     # @activities = PublicActivity::Activity.includes(:owner, :trackable)
     # @activities = @activities.where({trackable_type: ["Review", "Question"]} ).order("id desc")
     @posts = Review.all.includes(:user, knowledge: [:photos]) + Question.all.includes(:user, knowledge: [:photos])
-    @posts = @posts.sort_by{|post| post.created_at}
+    @posts = @posts.sort_by{|post| post.created_at}.reverse
     @posts = @posts.paginate(:page => params[:page], :per_page => 30)
   end
 end
