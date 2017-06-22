8dbf2f58ac9f71bbe837c4d574cef1a5930a9ca9
an-lee <an.lee.work@gmail.com>
2017-06-22 09:29:53 +0800

    change posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  require 'will_paginate/array'
   def index
     @activities = PublicActivity::Activity.includes(:owner, :trackable)
     @activities = @activities.where({trackable_type: ["Review", "Question"]} ).order("id desc")
+    @posts = Review.all.includes(:user, knowledge: [:photos]) + Question.all.includes(:user, knowledge: [:photos])
+    @posts = @posts.sort_by{|post| post.created_at}
+    @posts = @posts.paginate(:page => params[:page], :per_page => 30)
   end
 end
